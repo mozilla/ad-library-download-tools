79857b041f0f41fc60d205aae6f7d57a591fcb92
Jason Chuang <jchuang@mozilla.com>
2020-03-03 20:38:44 -0500

    Added QueueManager.

--- src/facebook_utils/queue.py
+++ src/facebook_utils/queue.py
@@ -8,41 +8,85 @@ class QueueManager:
 		self.verbose = verbose
 		self._db = QueueDB(db_folder = db_folder, verbose = False)
 
-	def get_next_task(self):
+	def get_next_active_task(self):
 		self._db.open()
 		task_count = self._db.get_active_task_count()
 		if task_count > 0:
-			this_task = self._db.get_next_active_task()
+			task = self._db.get_next_active_task()
 		else:
-			this_task = None
+			task = None
 		self._db.close()
-		return this_task
+		return task
 
 	def before_task(self):
 		self._db.open()
 
-	def start_task(self, task_key):
+	def run_start_task(self, task_key):
 		self._db.start_task(task_key)
 
-	def finish_task(self, task_key):
+	def run_finish_task(self, task_key):
 		self._db.finish_task(task_key)
 
-	def amend_task(self, task_key, finish_code, finish_log):
-		self._db.amend_task(task_key, finish_code, finish_log)
-
-	def get_task_as_dict(self, task_key):
-		return self._db.get_task_as_dict(task_key)
+	def run_amend_task(self, task_key):
+		self._db.amend_task(task_key)
 
 	def after_task(self):
 		self._db.close()
+
+	def amend_task(self, task_key, finish_code, finish_log):
+		self._db.open()
+		self._db.amend_task(task_key, finish_code, finish_log)
+		self._db.close()
+		if self.verbose:
+			print("[QueueManager] Amended task #{}".format(task_key))
+
+	def start_task(self, task_key):
+		self._db.open()
+		self._db.start_task(task_key)
+		self._db.close()
+		if self.verbose:
+			print("[QueueManager] Started task #{}".format(task_key))
+
+	def finish_task(self, task_key):
+		self._db.open()
+		self._db.finish_task(task_key)
+		self._db.close()
+		if self.verbose:
+			print("[QueueManager] Finished task #{}".format(task_key))
+
+	def cancel_task(self, task_key):
+		self._db.open()
+		self._db.cancel_task(task_key)
+		self._db.close()
+		if self.verbose:
+			print("[QueueManager] Cancelled task #{}".format(task_key))
+
+	def restart_task(self, task_key):
+		self._db.open()
+		self._db.restart_task(task_key)
+		self._db.close()
+		if self.verbose:
+			print("[QueueManager] Restarted task #{}".format(task_key))
+
+	def get_task_as_dict(self, task_key):
+		self._db.open()
+		task = self._db.get_task_as_dict(task_key)
+		self._db.close()
+		if self.verbose:
+			print("[QueueManager] Retrieved task #{} as a dict".format(task_key))
+		return task
 
 	def create_next_task(self, experiment_spec, split_spec, page_spec, attempt_spec, continuation):
 		self._db.open()
 		self._db.create_task(experiment_spec, split_spec, page_spec, attempt_spec, continuation)
 		self._db.close()
+		if self.verbose:
+			print("[QueueManager] Create a new task.")
 
 	def create_next_tasks(self, experiment_spec, split_specs, page_spec, attempt_spec, continuation):
 		self._db.open()
 		for split_spec in split_specs:
 			self._db.create_task(experiment_spec, split_spec, page_spec, attempt_spec, continuation)
 		self._db.close()
+		if self.verbose:
+			print("[QueueManager] Create {} new tasks.".format(len(split_specs)))
--- test/test_fb_queue.py
+++ test/test_fb_queue.py
@@ -2,36 +2,66 @@
 
 import facebook_utils
 
+import argparse
 import json
 import random
 
-def _print_json(label, data):
+START = "start"
+FINISH = "finish"
+CANCEL = "cancel"
+RESTART = "restart"
+NEXT = "next"
+CREATE = "create"
+
+EXPERIMENT_TYPES = ["us", "uk", "ca", "de", "eu"]
+
+def print_json(label, data):
 	print("[{}]".format(label))
 	print(json.dumps(data, indent = 2))
 	print()
 
-queue_manager = facebook_utils.QueueManager(db_folder = "../db/test", verbose = True)
+parser = argparse.ArgumentParser()
+parser.add_argument("command", choices = [START, FINISH, CANCEL, RESTART, NEXT, CREATE], type = str)
+parser.add_argument("value", help = "task_key or number of records", type = int, nargs = "?")
+args = parser.parse_args()
 
-for experiment_type in ["us", "uk", "de"]:
-	task_manager = facebook_utils.TaskManager(verbose = True)
-	experiment_spec = task_manager.create_experiment(experiment_type)
-	split_specs = task_manager.create_splits(experiment_spec)
-	page_spec = task_manager.init_page()
-	attempt_spec = task_manager.init_attempt()
-	continuation = task_manager.init_continuation()
-	queue_manager.create_next_tasks(experiment_spec, split_specs, page_spec, attempt_spec, continuation)
+queue = facebook_utils.QueueManager(db_folder = "../db/test", verbose = True)
 
-next_task = queue_manager.get_next_task()
-_print_json("next_task", next_task)
+command = args.command
 
-task_key = next_task["task_key"]
-queue_manager.before_task()
-queue_manager.start_task(task_key)
-queue_manager.finish_task(task_key)
+# Database operations
+if command == START:
+	task_key = args.value
+	queue.start_task(task_key)
+if command == FINISH:
+	task_key = args.value
+	queue.finish_task(task_key)
+	finish_code = random.randint(0, 100)
+	finish_log = {
+		"access_token": "{:06d}".format(random.randint(0, 999999))
+	}
+	queue.amend_task(task_key, finish_code, finish_log)
+if command == CANCEL:
+	task_key = args.value
+	queue.cancel_task(task_key)
+if command == RESTART:
+	task_key = args.value
+	queue.restart_task(task_key)
 
-finish_code = random.randint(0, 100)
-finish_log = {
-	"access_token": "{:06d}".format(random.randint(0, 999999))
-}
-queue_manager.amend_task(task_key, finish_code, finish_log)
-queue_manager.after_task()
+# Check database status
+if command == NEXT:
+	task = queue.get_next_active_task()
+	print_json("next_active_task", task)
+
+# Insert into database
+if command == CREATE:
+	rows = min(10, args.value)
+	for row in range(0, rows):
+		experiment_type = EXPERIMENT_TYPES[random.randint(0, len(EXPERIMENT_TYPES))]
+		task_manager = facebook_utils.TaskManager(verbose = True)
+		experiment_spec = task_manager.create_experiment(experiment_type)
+		split_specs = task_manager.create_splits(experiment_spec)
+		page_spec = task_manager.init_page()
+		attempt_spec = task_manager.init_attempt()
+		continuation = task_manager.init_continuation()
+		queue.create_next_tasks(experiment_spec, split_specs, page_spec, attempt_spec, continuation)
