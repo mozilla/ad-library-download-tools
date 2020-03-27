8f66dc9993ea379783bcfe3b1faab607397b3b7f
Jason Chuang <jchuang@mozilla.com>
2020-03-27 10:12:35 -0400

    Added script to save Google service account key.

---
+++ src/g_copy_service_account_key.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import google_utils
+import argparse
+import os
+import shutil
+import stat
+
+parser = argparse.ArgumentParser(
+	usage = "Copy your Google Service Account key to prefs folder."
+)
+parser.add_argument("service_account_key", help = "Google Service Account Key (as a JSON file)", type = str)
+args = parser.parse_args()
+
+download_helper = google_utils.DownloadHelper()
+src_path = args.service_account_key
+dest_path = download_helper.get_service_account_key_path()
+
+print("Copying from {} to {}...".format(src_path, dest_path))
+shutil.copyfile(src_path, dest_path)
+
+print("Setting file permissions to 600...")
+os.chmod(dest_path, stat.S_IRUSR | stat.S_IWUSR)
